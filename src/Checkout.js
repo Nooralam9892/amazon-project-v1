009ce910892e9c5c6b914071c367900dc86a5335
SNA2003 <88981459+SNA2003@users.noreply.github.com>
2021-11-06 19:31:28 +0530

    init

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ function Checkout() {
     return (
         <div className="checkout">
             <div className="checkout--left">
-                <img className="chekcout--adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/MAI/Sweepstakes/Nov/MEDH_SWM_1500x200.jpg" alt="avd" />
+                <img className="chekcout--adv" src="https://www.amazon.in/gp/prime/&ref_=IN_Prime_Lepton_BAU_Annual_Acquisition_SWM_Desktop/?ie=UTF8&ref_=nav_swm_IN-Prime_Lepton_BAU_Annual_Acquisition_SWM_Desktop&pf_rd_p=b7094bcb-f51e-4fe1-9caf-65d131c53b43&pf_rd_s=nav-sitewide-msg&pf_rd_t=4201&pf_rd_i=navbar-4201&pf_rd_m=A21TJRUUN4KGV&pf_rd_r=E33CW7WHBE8QCYK11JNK" alt="avd" />
                 <div>
                     <h3 className="checkout--title">Your Shoping Details</h3>
                 </div>
